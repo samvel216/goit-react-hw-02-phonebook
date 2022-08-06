00fdc937c680e4db2bcc7695cbdbcea0a2e6fcd4
samvel216 <75182258+samvel216@users.noreply.github.com>
2022-08-06 14:59:06 +0300

    fdf

--- src/components/Filter/filter.jsx
+++ src/components/Filter/filter.jsx
@@ -1,4 +1,4 @@
-import PropTypes from 'prop-types';
+import propTypes from 'prop-types';
 import styles from './Filter.module.css';
 
 export function Filter({ value, onChange }) {
@@ -15,7 +15,7 @@ export function Filter({ value, onChange }) {
   );
 }
 
-Filter.PropTypes = {
-  value: PropTypes.string,
-  onChange: PropTypes.func,
+Filter.propTypes = {
+  value: propTypes.string,
+  onChange: propTypes.func,
 };
